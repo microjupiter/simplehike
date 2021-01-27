456089188c9030fd1d43b78d89430712634e33b6
Kyle Wilson <kyle@hinterlandstills.com>
2021-01-27 12:43:50 -0600

    validate presence and uniqueness of username/pass

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,11 @@
+require './config/enviorment'
+
 class ApplicationController < ActionController::Base
+
+  configure do 
+    enable :sessions
+    set :public_folder, 'public'
+    set :views, 'app/views'
+    set :session_secret, "hotdog"
+    end
 end
--- app/models/hiker.rb
+++ app/models/hiker.rb
@@ -1,4 +1,7 @@
 class Hiker < ApplicationRecord
+  has_secure_password
+  validates_presence_of :username, :password_digest
+  validates :username, uniqueness: true
   has_many :reviews
   has_many :trails, through: :reviews
 end
