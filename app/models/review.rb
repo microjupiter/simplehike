77375414750cdc6c17602cee7d54a01b21f85c80
Kyle Wilson <kyle@hinterlandstills.com>
2021-02-03 11:31:59 -0600

    added ratyrate gem

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,23 +1,24 @@
 class Review < ApplicationRecord
   belongs_to :user
   belongs_to :trail
+  ratyrate_rateable 'trail_rating'
 
-  REVIEW_DIFFICULTY = ["Easy", "Moderate", "Hard"]
-  REVIEW_RATING = ["1", "2", "3", "4", "5"]
+  # REVIEW_DIFFICULTY = ["Easy", "Moderate", "Hard"]
+  # REVIEW_RATING = ["1", "2", "3", "4", "5"]
 
-  validates :content, length: { maximum: 500 }
-  validate :difficulty_is_valid?
-  validate :rating_is_valid?
+  # validates :content, length: { maximum: 500 }
+  # validate :difficulty_is_valid?
+  # validate :rating_is_valid?
 
-  def difficulty_is_valid?
-    if !REVIEW_DIFFICULTY.include?(self.difficulty)
-      self.errors.add(:difficulty, "must select difficulty to submit")
-    end
-  end
+  # def difficulty_is_valid?
+  #   if !REVIEW_DIFFICULTY.include?(self.difficulty)
+  #     self.errors.add(:difficulty, "must select difficulty to submit")
+  #   end
+  # end
 
-  def rating_is_valid?
-    if !REVIEW_RATING.include?(self.rating)
-      self.errors.add(:rating, "must select a rating to submit")
-    end
-  end
+  # def rating_is_valid?
+  #   if !REVIEW_RATING.include?(self.rating)
+  #     self.errors.add(:rating, "must select a rating to submit")
+  #   end
+  # end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,27 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_02_02_223938) do
+ActiveRecord::Schema.define(version: 2021_02_03_171923) do
+
+  create_table "average_caches", force: :cascade do |t|
+    t.integer "rater_id"
+    t.string "rateable_type"
+    t.integer "rateable_id"
+    t.float "avg", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["rateable_type", "rateable_id"], name: "index_average_caches_on_rateable"
+    t.index ["rater_id"], name: "index_average_caches_on_rater_id"
+  end
+
+  create_table "overall_averages", force: :cascade do |t|
+    t.string "rateable_type"
+    t.integer "rateable_id"
+    t.float "overall_avg", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["rateable_type", "rateable_id"], name: "index_overall_averages_on_rateable"
+  end
 
   create_table "parks", force: :cascade do |t|
     t.string "location"
@@ -18,6 +38,18 @@ ActiveRecord::Schema.define(version: 2021_02_02_223938) do
     t.integer "trail_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "rating_caches", force: :cascade do |t|
+    t.string "cacheable_type"
+    t.integer "cacheable_id"
+    t.float "avg", null: false
+    t.integer "qty", null: false
+    t.string "dimension"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["cacheable_id", "cacheable_type"], name: "index_rating_caches_on_cacheable_id_and_cacheable_type"
+    t.index ["cacheable_type", "cacheable_id"], name: "index_rating_caches_on_cacheable"
   end
 
   create_table "reviews", force: :cascade do |t|
