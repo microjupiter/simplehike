699da4d120a0b6d2d292b655c75a723ebbee6162
Kyle Wilson <kyle@hinterlandstills.com>
2021-02-05 12:54:46 -0600

    app: ind. park page shows reviews now for that park

--- app/controllers/trails_controller.rb
+++ app/controllers/trails_controller.rb
@@ -2,6 +2,8 @@ class TrailsController < ApplicationController
 
 def show
   @trail = Trail.find_by_id(params[:id])
+  @reviews = Review.where(:trail_id == @trail.id)
+  @user_reviewed = User.find_by(params[:id])
 end
 
 def index
